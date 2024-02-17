428220398410d9a38233c2e3794ca912328efd4a
Roland-Jephtha <jephtroland832@gmail.com>
2024-02-16 20:14:18 -0800

    hello

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -163,7 +163,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR/ 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 
 STATICFILES_DIRS =[
